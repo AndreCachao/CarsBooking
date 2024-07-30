27123aa0303e1969092a5cb7c51f2dcc88c51cfb
André Cachão <andre.cachao@gmail.com>
2024-07-31 00:52:59 +0100

    Routes (Undone)

---
+++ src/app/home/home.component.ts
@@ -0,0 +1,21 @@
+import { Component } from '@angular/core';
+import { MatTab, MatTabChangeEvent, MatTabGroup } from '@angular/material/tabs';
+
+@Component({
+  selector: 'home',
+  standalone: true,
+  imports: [],
+  styleUrls: ['home.style.scss'],
+  template: `
+    <div class="container">
+      <div class="content">
+        <h1>Welcome to Car Booking Service</h1>
+        <p>Use the tabs to navigate through the sections.</p>
+      </div>
+      ewaea
+    </div>
+  `,
+})
+export class HomeComponent {
+  title = 'my-angular-app';
+}
